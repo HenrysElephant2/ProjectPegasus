d15e36f005682a80e487026b6f0bcaa4676f2220
Alex Burnley <albu5828@colorado.edu>
2019-03-11 15:26:55 -0600

    Added basic Component struct

---
+++ Component.h
@@ -0,0 +1,17 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+//#include <glm/glm.hpp>
+
+struct Component {
+	int ownerID; // entityID for the entity that owns this component
+};
+
+
+// struct Transform:Component {
+// 	glm::vec3 position;
+// 	//add rotation and scale, not sure best way to store these
+
+// };
+
+#endif
