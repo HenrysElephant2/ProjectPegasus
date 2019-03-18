cca173e02b59fe75da3159d046d54f71ae9ef286
Alex Burnley <albu5828@colorado.edu>
2019-03-18 15:59:05 -0600

    Added Renderable Component, and base for several other components

--- Component.h
+++ Component.h
@@ -7,17 +7,57 @@
 
 struct Component {
 	int ownerID; // entityID for the entity that owns this component
+	Component(int owner){ownerID = owner}
 };
 
 
 struct Transform:Component {
 	glm::vec3 position;
-	//add rotation and scale, not sure best way to store these
-
+	glm::vec3 forward;
+	glm::vec3 up;
+	float scale;
+	Transform(glm::vec3& position_in, glm::vec3& forward_in, glm::vec3& up_in, float scale_in, int ownerID) : Component(ownerID)
+	{
+		position = position_in;
+		forward = forward_in;
+		up = up_in;
+		scale = scale_in;
+	}
 };
 
 struct Renderable:Component {
-
+	GLuint VBO;
+	GLuint IBO;
+	int numVertices;
+	int program; // index into shadermanager
+	Material material;
+	Renderable(GLuint VBO_in, GLuint IBO_in, int vertex_Count, int program_in, Material & material_in, int ownerID):Component(ownerID)
+	{
+		VBO = VBO_in;
+		IBO = IBO_in;
+		numVertices = vertex_Count;
+		program = program_in;
+		material = material_in;
+	}
 };
+
+struct Physics:Component {
+	glm::vec3 velocity;
+	float mass;
+	bool freeze; // add this?
+}
+
+struct Collider:Component {
+
+}
+
+struct Trigger:Component {
+	float radius;
+	BasicMessage m;
+}
+
+struct Grapple:Component {
+
+}
 
 #endif
